56f01716a993821f41d33723c939361b854c8501
suspost <jojo1jospeh@gmail.com>
2026-04-27 01:07:41 +0500

    Delete TSIS directory

--- TSIS/TSIS1/config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-DB_CONFIG = {
-    "dbname": "phonebook_db", # Имя вашей базы данных
-    "user": "postgres",       # Имя пользователя БД
-    "password": "arai 123", # ВАШ ПАРОЛЬ
-    "host": "localhost",
-    "port": "5432"
-}
